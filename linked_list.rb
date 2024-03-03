6d1cd2153d199487474a680f37f2a99b748a70cd
Ali Aizad <aliaizad72@gmail.com>
2024-03-04 00:40:32 +0800

    Finished at(index) method

--- linked_list.rb
+++ linked_list.rb
@@ -41,6 +41,31 @@ class LinkedList
       i
     end
   end
+
+  def tail
+    if size == 0
+      nil
+    elsif size == 1
+      @head
+    else
+      cursor = @head
+      cursor = cursor.next_node until cursor.next_node.nil?
+      cursor
+    end
+  end
+
+  def at(index)
+    case index
+    when 0
+      @head
+    when -1
+      tail
+    else
+      cursor = @head
+      index.times { cursor = cursor.next_node }
+      cursor
+    end
+  end
 end
 
 # class Node that contains a value and a link
@@ -58,4 +83,3 @@ list.prepend(3)
 list.prepend(2)
 list.append(4)
 
-p list.size
